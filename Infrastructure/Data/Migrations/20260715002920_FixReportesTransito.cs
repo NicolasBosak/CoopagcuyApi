e8aad52a42133b8a19044a9c5e581b1cce3fb608
Nicolas Casamen <sebastian.casamen@udla.edu.ec>
2026-07-14 19:29:51 -0500

    feat: Migraciones

---
+++ Infrastructure/Data/Migrations/20260715002920_FixReportesTransito.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoopagcuyApi.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixReportesTransito : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
